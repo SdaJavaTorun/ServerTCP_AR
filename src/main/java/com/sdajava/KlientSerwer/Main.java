b8c7a7a3d08bb8cd954d598fff841e95cd584556
grovas <arczir@gmail.com>
2017-04-21 20:48:09 +0200

    1st commit

---
+++ src/main/java/com/sdajava/KlientSerwer/Main.java
@@ -0,0 +1,11 @@
+package com.sdajava.KlientSerwer;
+
+import java.io.*;
+import java.net.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(4444);
+    }
+}
